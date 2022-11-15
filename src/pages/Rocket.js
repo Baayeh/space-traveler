a2e628874edadc5beeab94a778c8035a37bada50
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-15 15:29:35 +0000

    update Rockets.js

---
+++ src/pages/Rocket.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchAllRockets } from '../redux/rocket/rockets';
+
+const Rocket = () => (
+  <div>
+    all rockets test
+  </div>
+);
+// const rockets = useSelector((state) => state.rocket);
+// const dispatch = useDispatch();
+
+// useEffect(() => {
+//   dispatch(fetchAllRockets());
+// }, []);
+// console.log(rockets);
+
+export default Rocket;
