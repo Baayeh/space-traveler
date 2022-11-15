17b73d04a3cf5c42e06a8d45b80803286287f250
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-15 15:30:06 +0000

    add rocketsApi.js

---
+++ src/redux/endpoints/rocketsApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.spacexdata.com/v3/rockets';
+
+const rocketFetch = async () => {
+  const response = await axios.get(BASE_URL);
+  return response.data;
+  // return response.data.map((item) => ({
+  //   id: item.id,
+  //   rockets_name: item.rocket_name,
+  //   description: item.description,
+  //   flickr_images: item.flickr_images[0],
+  // }));
+};
+
+export default rocketFetch;
