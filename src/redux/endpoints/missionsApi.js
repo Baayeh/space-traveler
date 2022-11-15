258312b5132608a73d68ac396e2ff6f1cfbb6237
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-15 15:36:10 +0000

    Implement join mission functionality

--- src/redux/endpoints/missionsApi.js
+++ src/redux/endpoints/missionsApi.js
@@ -4,7 +4,12 @@ const URL = 'https://api.spacexdata.com/v3/missions';
 
 const fetchAllMissions = async () => {
   const res = await axios.get(URL);
-  return res.data;
+  return res.data.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+    reserved: false,
+  }));
 };
 
 export default fetchAllMissions;
--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -3,7 +3,7 @@ import { createAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import fetchAllMissions from '../endpoints/missionsApi';
 
 // Action creator
-const missionFetch = createAction('missions/fetchmissions');
+const missionFetch = createAction('mission/fetchmissions');
 
 // state
 const initialState = {
@@ -15,19 +15,39 @@ const initialState = {
 // Thunk Creator
 export const fetchMission = createAsyncThunk(missionFetch, async () => {
   const missions = await fetchAllMissions();
-  return missions.map((mission) => (
-    {
-      mission_id: mission.mission_id,
-      mission_name: mission.mission_name,
-      description: mission.description,
-    }
-  ));
+  return missions;
 });
 
 // Reducer
 const missionSlice = createSlice({
   name: 'mission',
   initialState,
+  reducers: {
+    reserveMission: (state, action) => ({
+      ...state,
+      missions: state.missions.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return {
+            ...mission,
+            reserved: true,
+          };
+        }
+        return mission;
+      }),
+    }),
+    leaveMission: (state, action) => ({
+      ...state,
+      missions: state.missions.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return {
+            ...mission,
+            reserved: false,
+          };
+        }
+        return mission;
+      }),
+    }),
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchMission.pending, (state) => {
@@ -44,5 +64,7 @@ const missionSlice = createSlice({
       });
   },
 });
+
+export const { reserveMission, leaveMission } = missionSlice.actions;
 
 export default missionSlice.reducer;
