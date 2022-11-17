796fa0529479b4079394ba5e7997f3a47bd7efca
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-17 13:34:56 +0000

    Add app logo

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/spacex-logo.png';
 
 function NavBar() {
   return (
     <div className="nav_header_container">
-      <h2 className="logo">Space Travelers Hub</h2>
+      <div className="logo-header">
+        <img src={logo} alt="" className="logo" />
+        <h2 className="logo">Space Travelers Hub</h2>
+      </div>
 
       <nav className="nav_items">
         <ul className="nav-links">
