b5be0c97495d98d893bd45922c9af88b47753b1b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-11 14:50:32 +0000

    Update Missions component

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(20deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 3s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+};
